24aff4d8f052d8d54d07e6af686efe93723dfd3a
Jake Sorce <jake@instructure.com>
2014-07-08 14:22:56 -0600

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChadHydro::Application.config.secret_key_base = 'a3c274c137616d5e64782b3b4b86ab78f5690bb7a21e2e3b18710a16b4ba8a6aa834c38e711e25cb9a9051b5cd11b506422745e706471ec9c2221d22386c2101'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChadHydro::Application.config.session_store :cookie_store, key: '_chad-hydro_session'
