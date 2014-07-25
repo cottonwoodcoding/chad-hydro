f4f6b0e6d8ee8731b4fedb9bb6894ec35869faea
Jake Sorce <jake@instructure.com>
2014-07-24 21:25:34 -0600

    featured projects and menu changes

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,6 +2,7 @@ class AboutController < ApplicationController
   respond_to :html
 
   def index
+    @featured_products = ShopifyAPI::CustomCollection.where(handle: 'featured-products').first.products
     respond_with do |format|
         format.html { render :layout => false }
       end
