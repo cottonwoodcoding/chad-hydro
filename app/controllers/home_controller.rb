fd14d1a0f837cd78b3f5e63aaa5358c1037719ed
Jake Sorce <jake@instructure.com>
2014-07-24 21:25:34 -0600

    featured projects and menu changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   respond_to :html
 
   def index
+    @featured_products = ShopifyAPI::CustomCollection.where(handle: 'featured-products').first.products
   end
 
 end
