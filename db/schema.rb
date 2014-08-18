238e1f7a18cee916a39d9982ef0b59184624b7fd
Jake Sorce <jakesorce@gmail.com>
2014-08-18 12:16:40 -0600

    remove styling for social media links
    i think it was fine close to the logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815233815) do
+ActiveRecord::Schema.define(version: 20140815232136) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
