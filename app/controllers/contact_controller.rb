03b74eef487156fffd0c1483714a50f7567601d5
Jake Sorce <jakesorce@gmail.com>
2014-09-29 14:06:16 -0600

    more settings stuff

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,10 +4,10 @@ class ContactController < ApplicationController
   def index
     f = Biggs::Formatter.new
     @address = f.format("us", # <= ISO alpha 2 code
-      :street     => Setting.setting_value('street address'),
-      :city       => Setting.setting_value('city'),
-      :zip        => Setting.setting_value('zip'),
-      :state      => Setting.setting_value('state') # <= state/province/region
+      :street     => Setting.setting_value('general-street-address'),
+      :city       => Setting.setting_value('general-city'),
+      :zip        => Setting.setting_value('general-zip'),
+      :state      => Setting.setting_value('general-state') # <= state/province/region
     )
     @days = %w(monday tuesday wednesday thursday friday saturday sunday)
   end
