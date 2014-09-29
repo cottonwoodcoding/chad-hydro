823bbc4bc513f6f6cf0cdc8b115408bb940454ec
Jake Sorce <jakesorce@gmail.com>
2014-09-29 10:47:39 -0600

    commit to fix fonts prod

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module ChadHydro
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.cache_store = :dalli_store
