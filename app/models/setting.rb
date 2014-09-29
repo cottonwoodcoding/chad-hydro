8264b1fa6964ad781ed85887aab109559dfcbc81
Jake Sorce <jakesorce@gmail.com>
2014-09-29 14:19:51 -0600

    more settings stuff

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,6 +1,6 @@
 class Setting < ActiveRecord::Base
   attr_accessible :name, :value
-  validates :name, :value, presence: true
+  validates :name, :value, presence: true, null: true
   validates :name, uniqueness: true
 
   def self.setting_value(setting_name)
